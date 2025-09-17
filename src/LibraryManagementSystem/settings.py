73f263751dc1be76152fbf31083649ce5265e0b7
wanthinnn <thienlai159@gmail.com>
2025-09-17 18:38:58 +0700

    deploy cloud db postgresql

--- src/LibraryManagementSystem/settings.py
+++ src/LibraryManagementSystem/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@kl970*(&4#b427!q380o_0a@y1igby2+5ra&knin!@kf0xbuq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
