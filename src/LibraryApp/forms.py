cb5f464de8015b115ee9e07e4ab6d4825d969732
Lại Quan Thiên <Thienlai159@gmail.com>
2025-12-29 17:10:07 +0700

    feat: add Django forms for user authentication, reader management, and book borrowing.

--- src/LibraryApp/forms.py
+++ src/LibraryApp/forms.py
@@ -159,10 +159,11 @@ class ReaderForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Set ngày lập thẻ mặc định là hôm nay
+        # Set giá trị mặc định khi tạo mới
         if not self.instance.pk:
             from django.utils import timezone
             self.initial['card_creation_date'] = timezone.now().date()
+            self.initial['is_active'] = True  # Độc giả mới mặc định được kích hoạt
     
     def validate_reader_data(self):
         """Kiểm tra toàn bộ dữ liệu lập thẻ độc giả theo QĐ1"""
